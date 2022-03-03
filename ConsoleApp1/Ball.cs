23b35f0d81546830f9e09b2f54ce3523b4db537d
yakir barzilai <notcooldudet@gmail.com>
2022-03-03 15:50:32 +0200

    he he

---
+++ ConsoleApp1/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Ball
+    {
+        int size;
+        string color;
+        string type;
+        public int get_size()
+        {
+            return this.size;
+        }
+        public void set_size(int size)
+        {
+            this.size = size;
+        }
+        public string get_color()
+        {
+            return this.color;
+        }
+        public void set_color(string color)
+        {
+            this.color = color;
+        }
+        public string get_type()
+        {
+            return this.type;
+        }
+        public void set_type(string type)
+        {
+            this.type = type;
+        }
+    }
+}
+    
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ball my_new_ball = new Ball();
+            Console.WriteLine("please enter your ball detaile");
+            Console.WriteLine("size?");
+            my_new_ball.set_size(int.Parse(Console.ReadLine()));
+            Console.WriteLine("color?");
+            my_new_ball.set_color(Console.ReadLine());
+            Console.WriteLine("type?");
+            my_new_ball.set_type(Console.ReadLine());
+        }
+    }
+}
